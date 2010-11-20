06318ef5dc96eb071967b4a2c65712243daa0ec0
Andrew Oberstar <ajoberstar@gmail.com>
2010-11-20 16:28:01 -0600

    Consolidating external build logic into build.gradle.

--- config/codenarc/codenarc.groovy
+++ config/codenarc/codenarc.groovy
@@ -3,14 +3,22 @@ ruleset {
 	ruleset('rulesets/braces.xml')
 	ruleset('rulesets/concurrency.xml')
 	ruleset('rulesets/design.xml')
-	ruleset('rulesets/exceptions.xml')
+	ruleset('rulesets/exceptions.xml') {
+		'ReturnNullFromCatchBlock' {
+			doNotApplyToFileNames = 'CueSheetDaoImpl.groovy'
+		}
+	}
 	ruleset('rulesets/generic.xml') {
 		'StatelessClass' {
 			doNotApplyToFileNames = '*Test.groovy'
 		}
 	}
 	ruleset('rulesets/imports.xml')
-	ruleset('rulesets/junit.xml')
+	ruleset('rulesets/junit.xml') {
+		'JUnitStyleAssertions' {
+			enabled = false
+		}
+	}
 	ruleset('rulesets/logging.xml')
 	ruleset('rulesets/naming.xml') {
 		'FieldName' {
