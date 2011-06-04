0043aac987c6759d479375c687afbf34315cdbd2
Andrew Oberstar <ajoberstar@gmail.com>
2011-06-04 12:47:39 -0500

    Removing windows specific file info from logback config.

--- src/main/resources/logback.groovy
+++ src/main/resources/logback.groovy
@@ -1,24 +1,13 @@
-import static ch.qos.logback.classic.Level.*;
+import static ch.qos.logback.classic.Level.*
 import ch.qos.logback.core.ConsoleAppender
 import ch.qos.logback.core.FileAppender
 import ch.qos.logback.classic.encoder.PatternLayoutEncoder
 
-def appenderList = ["CONSOLE", "FILE"]
-
-appender("CONSOLE", ConsoleAppender) {
+appender('CONSOLE', ConsoleAppender) {
 	encoder(PatternLayoutEncoder) {
-		pattern = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n"
+		pattern = '%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n'
 	}
 }
 
-appender("FILE", FileAppender) {
-	file = "C:\\Users\\Andy\\Desktop\\out.txt"
-	append = false
-	encoder(PatternLayoutEncoder) {
-		pattern = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n"
-	}
-}
-
-root(INFO, appenderList)
+root(INFO, ['CONSOLE')
 logger('org.springframework', WARN)
-logger('com.andrewoberstar.library.meta.dao.impl.FreeDbJdbcDaoImpl', DEBUG)
