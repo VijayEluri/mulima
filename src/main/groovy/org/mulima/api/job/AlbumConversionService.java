69629ea28b93c7fd40dad887fe65c5b75538e912
Andrew Oberstar <ajoberstar@gmail.com>
2011-10-29 17:02:26 -0500

    Added javadoc to job classes.

--- src/main/groovy/org/mulima/api/job/AlbumConversionService.java
+++ src/main/groovy/org/mulima/api/job/AlbumConversionService.java
@@ -6,9 +6,30 @@ import java.util.concurrent.Future;
 
 import org.mulima.api.library.LibraryAlbum;
 
-
+/**
+ * A service to convert albums between formats.
+ * @author Andrew Oberstar
+ * @version 0.1.0
+ * @since 0.1.0
+ */
 public interface AlbumConversionService {
+	/**
+	 * Submits a conversion from {@code source} to
+	 * {@code dests}.
+	 * @param source the source album
+	 * @param dests the destination albums
+	 * @return a future representing the execution of the conversion
+	 */
 	Future<Void> submit(LibraryAlbum source, Set<LibraryAlbum> dests);
+	
+	/**
+	 * Shuts down the service.
+	 */
 	void shutdown();
+	
+	/**
+	 * Shuts the service down now.
+	 * @return runnables that were still executing
+	 */
 	List<Runnable> shutdownNow();
 }
--- src/main/groovy/org/mulima/internal/job/Step.java
+++ src/main/groovy/org/mulima/internal/job/Step.java
@@ -2,8 +2,32 @@ package org.mulima.internal.job;
 
 import java.util.concurrent.Callable;
 
+/**
+ * An object that will execute an action
+ * to perform a task.
+ * @author Andrew Oberstar
+ * @version 0.1.0
+ * @since 0.1.0
+ *
+ * @param <T> the return type of the step
+ */
 public interface Step<T> extends Callable<T>{
+	/**
+	 * Executes the step.
+	 * @return {@code true} if the step succeeded,
+	 * {@code false} otherwise
+	 */
 	boolean execute();
+	
+	/**
+	 * Gets the current status of the step.
+	 * @return the status
+	 */
 	Status getStatus();
+	
+	/**
+	 * Gets the outputs of the step.
+	 * @return the outputs
+	 */
 	T getOutputs();
 }
