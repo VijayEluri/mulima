c9f1cc992746fbf4980ba69c5333fd6c2109d6b0
Andrew Oberstar <ajoberstar@gmail.com>
2014-10-25 07:40:07 -0500

    New lein project.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to mulima
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/mulima/core_test.clj
@@ -0,0 +1,7 @@
+(ns mulima.core-test
+  (:require [clojure.test :refer :all]
+            [mulima.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
