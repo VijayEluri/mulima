f3fcb65039f0fd4dd52cdf533cbab4ba7ec6fb3a
Andrew Oberstar <ajoberstar@gmail.com>
2014-11-08 19:38:37 -0600

    Clean up flac read-tags!

--- src/mulima/tool.clj
+++ src/mulima/tool.clj
@@ -1,15 +1,22 @@
-(ns mulima.tool)
+(ns mulima.tool
+  (:require [me.raynes.conch :as sh]))
 
 (defprotocol Splitter
-  (split [opts image dest-dir]))
+  (split! [opts image dest-dir]))
 
 (defprotocol Joiner
-  (join [opts files dest-image]))
+  (join! [opts files dest-image]))
 
 (defprotocol Tagger
-  (write-tags [opts file])
-  (read-tags [opts file]))
+  (write-tags! [opts file])
+  (read-tags! [opts file]))
 
 (defprotocol Codec
-  (encode [opts source dest])
-  (decode [opts source dest]))
+  (encode! [opts source dest])
+  (decode! [opts source dest]))
+
+(defn cmd!
+  [path & args]
+  (let [writer (java.io.StringWriter.)]
+    (sh/run-command path args {:out writer})
+    (str writer)))
--- src/mulima/tool/flac.clj
+++ src/mulima/tool/flac.clj
@@ -1,16 +1,15 @@
 (ns mulima.tool.flac
-  (:require [mulima.tool :refer :all]
-            [me.raynes.conch :as sh]))
+  (:require [mulima.tool :as tool]))
 
 (defrecord FlacOpts
   [path
    compression-level])
 
 (extend-type FlacOpts
-  Codec
-  (encode [opts source dest]
+  tool/Codec
+  (encode! [opts source dest]
     nil)
-  (decode [opts source dest]
+  (decode! [opts source dest]
     nil))
 
 (defrecord MetaflacOpts
@@ -19,14 +18,11 @@
 (def ^:private tag-pattern #"(?m)^\s*comment\[[0-9]+\]:\s+(\w+)=(.*)\s*$")
 
 (extend-type MetaflacOpts
-  Tagger
-  (write-tags [opts file]
+  tool/Tagger
+  (write-tags! [opts file tags]
     nil)
-  (read-tags [opts file]
-    (sh/let-programs [cmd (get opts :path "metaflac")]
-      (let [writer (java.io.StringWriter.)]
-        (cmd "--list" "--block-type=VORBIS_COMMENT" file {:out writer})
-        (->> (str writer)
-             (re-seq tag-pattern)
-             (map (fn [[m k v]] [k v]))
-             (into {}))))))
+  (read-tags! [opts file]
+    (->> (tool/cmd! (:path opts) "--list" "--block-type=VORBIS_COMMENT" file)
+         (re-seq tag-pattern)
+         (map (fn [[m k v]] [k v]))
+         (into {}))))
