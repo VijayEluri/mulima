35126228149476930bcf8826db41d4a936e9dcfd
Andrew Oberstar <ajoberstar@gmail.com>
2015-07-25 17:45:27 -0500

    Updating dependencies.

--- project.clj
+++ project.clj
@@ -3,16 +3,17 @@
   :url "https://github.com/ajoberstar/mulima"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [org.clojure/tools.logging "0.3.1"]
 
                  ;; reloaded
-                 [org.clojure/tools.namespace "0.2.7"]
+                 [org.clojure/tools.namespace "0.2.10"]
 
                  ;; io
                  [info.hoetzel/clj-nio2 "0.1.1"]
                  [me.raynes/conch "0.8.0"]
-                 [org.clojure/data.json "0.2.5"]
+                 [org.clojure/data.json "0.2.6"]
                  [org.clojure/data.xml "0.0.8"]]
-  :plugins [[cider/cider-nrepl "0.7.0"]])
+  :plugins [[cider/cider-nrepl "0.7.0"]
+            [lein-ancient "0.6.7"]])
