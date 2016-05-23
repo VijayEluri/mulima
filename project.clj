e0d9d09b1e8f8a1ca56554fec5b8ff71dad6d43b
Andrew Oberstar <ajoberstar@gmail.com>
2016-05-22 19:05:09 -0500

    Update deps and use ike.cljj instead of custom

--- project.clj
+++ project.clj
@@ -3,18 +3,20 @@
   :url "https://github.com/ajoberstar/mulima"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.async "0.2.374"]
                  [org.clojure/tools.logging "0.3.1"]
 
                  ;; reloaded
-                 [org.clojure/tools.namespace "0.2.10"]
+                 [org.clojure/tools.namespace "0.2.11"]
+
+                 ;; util
+                 [ike/ike.cljj "0.2.2"]
 
                  ;; io
-                 [info.hoetzel/clj-nio2 "0.1.1"]
                  [me.raynes/conch "0.8.0"]
                  [org.clojure/data.json "0.2.6"]
-                 [org.clojure/data.xml "0.0.8"]]
-  :plugins [[cider/cider-nrepl "0.7.0"]
-            [lein-ancient "0.6.7"]]
-  :profiles {:dev {:dependencies [[org.clojure/test.check "0.7.0"]]}})
+                 [org.clojure/data.xml "0.1.0-beta1"]]
+  :plugins [[cider/cider-nrepl "0.12.0"]
+            [lein-ancient "0.6.10"]]
+  :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}})
