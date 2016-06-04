3e220117c827c606f6d530dbce09d38571f1aca4
Andrew Oberstar <ajoberstar@gmail.com>
2016-06-04 17:42:21 -0500

    Added another for missing artwork.

--- src/mulima/core.clj
+++ src/mulima/core.clj
@@ -2,7 +2,8 @@
   (:require [mulima.meta :as meta]
             [ike.cljj.file :as file]
             ;; just for the collreduce impl of stream
-            [ike.cljj.stream]))
+            [ike.cljj.stream :as stream]
+            [clojure.string :as str]))
 
 (defn- incomplete-meta?
   [meta]
@@ -20,6 +21,27 @@
                  (map str))]
     (into [] xf children)))
 
+(defn- album-dir?
+  [path]
+  (and (file/dir? path)
+       (file/exists? (.resolve path "album.xml"))))
+
+(defn- no-artwork?
+  [dir]
+  (let [children (file/list dir)
+        xf (comp (map #(-> % .getFileName str (str/split #"\.") last))
+                 (filter #{"jpg" "png"}))]
+    (empty? (sequence xf (stream/stream-seq children)))))
+
+(defn find-missing-artwork
+  [root-dir]
+  (let [children (file/walk root-dir)
+        xf (comp (filter album-dir?)
+                 (filter no-artwork?)
+                 (map str))]
+    (into [] xf children)))
+
 #_(doseq [file (find-incomplete-meta "F:\\Music\\flac-rips2")] (println file))
+#_(doseq [file (find-missing-artwork "F:\\Music\\flac-rips2")] (println file))
 #_(meta/parse "F:\\Music\\flac-rips2\\Adele\\19\\album.xml")
 #_(println *e)
