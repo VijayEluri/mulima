0bf55920ed0a9ff28566b8a628c4d1c1e5d5c58a
Andrew Oberstar <ajoberstar@gmail.com>
2017-11-19 17:20:00 -0600

    restart

--- src/main/clojure/mulima/config.clj
+++
@@ -1,5 +0,0 @@
-(ns mulima.config
-  (:require [clojure.edn :as edn]
-            [ike.cljj.file :as file]
-            [mulima.tool :as tool]
-            [mulima.tool.flac :as flac]))
--- src/main/clojure/mulima/hash.clj
+++
@@ -1,9 +0,0 @@
-(ns mulima.hash
-  (:require [ike.cljj.file :as file]
-            [clojure.java.io :as io])
-  (:import [org.apache.commons.codec.digest DigestUtils]))
-
-(defn sha1 [path]
-  (with-open [is (io/input-stream path)]
-    ; (DigestUtils/sha1Hex is)
-    (DigestUtils/sha256Hex is)))
