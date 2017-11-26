86b8e2e546d43df842ce102a7b2e689628422465
Andrew Oberstar <ajoberstar@gmail.com>
2017-11-25 19:50:30 -0600

    basic ingestion of flac tags

--- src/main/clojure/mulima/core.clj
+++ src/main/clojure/mulima/core.clj
@@ -4,7 +4,10 @@
             [clojure.java.io :as io]
             [clojure.string :as string]
             [datomic.api :as datomic]
-            [ike.cljj.file :as file])
+            [ike.cljj.file :as file]
+            [ike.cljj.stream :as stream]
+            [mulima.tag :as tag]
+            [clojure.core.async :as async])
   (:import [java.nio.file Files LinkOption]
            [java.util Date]
            [java.io PushbackReader]))
@@ -21,7 +24,7 @@
     (println "Starting DB")
     (datomic/create-database (:uri db))
     (let [conn (datomic/connect (:uri db))]
-      (datomic/transact conn schema)
+      @(datomic/transact conn schema)
       (assoc db :connection conn)))
   (stop [db]
     (println "Stopping DB")
@@ -30,12 +33,27 @@
 (defn new-database [uri]
   (map->Database {:uri uri}))
 
+(defn extension-match [ext]
+  (fn [path]
+    (let [real-ext (-> path file/as-path .getFileName str (string/split #"\.") last)]
+      (= ext real-ext))))
+
 (defn scan-file [file]
-  {:file/path (str file)
-   :file/size (file/size file)
-   :file/modified (Date/from (.toInstant (Files/getLastModifiedTime file (into-array LinkOption []))))})
+  (let [tag-info (tag/parse file)
+        file-info {:mulima.file/path (str file)
+                   :mulima.file/size (file/size file)
+                   :mulima.file/modified (Date/from (.toInstant (Files/getLastModifiedTime file (into-array LinkOption []))))}]
+    (merge file-info tag-info)))
 
 (defn scan-dir [database dir]
   (with-open [stream (file/walk dir)]
-    (let [txs (into [] (map scan-file) stream)]
-      (datomic/transact (:connection database) txs))))
+    (let [xf (comp (filter file/file?)
+                   (filter (extension-match "flac"))
+                   (map scan-file)
+                   (partition-all 25))
+          tx-chan (async/chan)]
+      (async/pipeline-blocking 1 tx-chan xf (async/to-chan (into [] stream)))
+      (async/go-loop []
+        (when-let [txs (async/<! tx-chan)]
+          @(datomic/transact (:connection database) txs)
+          (recur))))))
