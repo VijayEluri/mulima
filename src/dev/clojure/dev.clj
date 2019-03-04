b5359bc6ba3f559e6d3df8923993fdc730569a14
Andrew Oberstar <ajoberstar@gmail.com>
2019-03-04 15:14:28 -0600

    garbage

--- src/dev/clojure/dev.clj
+++ src/dev/clojure/dev.clj
@@ -1,12 +1,4 @@
 (ns dev
   (:require [mulima.core :as core]
             [com.stuartsierra.component :as component]
-            [com.stuartsierra.component.repl :as repl :refer [set-init start stop reset]]
-            [datomic.api :as d]
-            [ike.cljj.file :as file]))
-
-(defn dev-system [_]
-  (component/system-map
-    :db (core/new-database "datomic:mem://mulima")))
-
-(repl/set-init dev-system)
+            [com.stuartsierra.component.repl :as repl :refer [set-init start stop reset]]))
--- src/main/clojure/mulima/core.clj
+++ src/main/clojure/mulima/core.clj
@@ -3,7 +3,6 @@
             [clojure.edn :as edn]
             [clojure.java.io :as io]
             [clojure.string :as string]
-            [datomic.api :as datomic]
             [ike.cljj.file :as file]
             [ike.cljj.stream :as stream]
             [mulima.tag :as tag]
@@ -11,49 +10,3 @@
   (:import [java.nio.file Files LinkOption]
            [java.util Date]
            [java.io PushbackReader]))
-
-(def schema (with-open [reader (-> "schema.edn"
-                                   io/resource
-                                   io/reader
-                                   PushbackReader.)]
-              (edn/read reader)))
-
-(defrecord Database [uri connection]
-  component/Lifecycle
-  (start [db]
-    (println "Starting DB")
-    (datomic/create-database (:uri db))
-    (let [conn (datomic/connect (:uri db))]
-      @(datomic/transact conn schema)
-      (assoc db :connection conn)))
-  (stop [db]
-    (println "Stopping DB")
-    (datomic/release (:connection db))))
-
-(defn new-database [uri]
-  (map->Database {:uri uri}))
-
-(defn extension-match [ext]
-  (fn [path]
-    (let [real-ext (-> path file/as-path .getFileName str (string/split #"\.") last)]
-      (= ext real-ext))))
-
-(defn scan-file [file]
-  (let [tag-info (tag/parse file)
-        file-info {:mulima.file/path (str file)
-                   :mulima.file/size (file/size file)
-                   :mulima.file/modified (Date/from (.toInstant (Files/getLastModifiedTime file (into-array LinkOption []))))}]
-    (merge file-info tag-info)))
-
-(defn scan-dir [database dir]
-  (with-open [stream (file/walk dir)]
-    (let [xf (comp (filter file/file?)
-                   (filter (extension-match "flac"))
-                   (map scan-file)
-                   (partition-all 25))
-          tx-chan (async/chan)]
-      (async/pipeline-blocking 1 tx-chan xf (async/to-chan (into [] stream)))
-      (async/go-loop []
-        (when-let [txs (async/<! tx-chan)]
-          @(datomic/transact (:connection database) txs)
-          (recur))))))
